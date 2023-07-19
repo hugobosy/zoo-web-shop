b633edf70fc731db06ae9c8837a5c8dff1cd876a
Hubert <h.kliszcz@nebucode.com>
2023-07-19 14:50:01 +0200

    add size styles

--- src/components/templates/HomePage/HomePage.tsx
+++ src/components/templates/HomePage/HomePage.tsx
@@ -1,5 +1,7 @@
 import { Typography } from '@/components/base/typography/Typography';
 
 export const HomePage = () => {
-  return <Typography as="h1" text="Naglowek" weight="700" />;
+  return (
+    <Typography as="h1" text="Naglowek" weight="700" capitalization="3xl" />
+  );
 };
