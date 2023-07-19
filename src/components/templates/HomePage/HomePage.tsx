2ac9ad2c838ef8be57fa8c8e09d4ae2a66b310ef
Hubert <h.kliszcz@nebucode.com>
2023-07-19 14:21:01 +0200

    add weight style

--- src/components/templates/HomePage/HomePage.tsx
+++ src/components/templates/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Typography } from '@/components/base/typography/Typography';
 
 export const HomePage = () => {
-  return <Typography as="h1" text="Naglowek" />;
+  return <Typography as="h1" text="Naglowek" weight="700" />;
 };
