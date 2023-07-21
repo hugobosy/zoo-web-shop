d266fc28ebc6420d849c0d052c98af686af04c9a
Hubert <h.kliszcz@nebucode.com>
2023-07-21 14:18:40 +0200

    Add image logo, and add first header style

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,4 +1,24 @@
-import styles from '/Header.module.scss';
+import styles from './Header.module.scss';
+import Image from 'next/image';
+import nameLogo from '@/assets/image/logo-name.png';
+import dogLogo from '@/assets/image/logo-dog.png';
 export const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <>
+      <div className={styles.bar}></div>
+      <div className={styles.header}>
+        <div className={styles['header-logo']}>
+          <Image src={nameLogo} alt="Lakrama" width={200} height={100} />
+          <Image src={dogLogo} alt="Lakrama" width={200} height={100} />
+        </div>
+        <div>
+          <h1>szukajka</h1>
+        </div>
+        <div>
+          <h1>logowanie</h1>
+        </div>
+      </div>
+      <div className={styles.menu}></div>
+    </>
+  );
 };
