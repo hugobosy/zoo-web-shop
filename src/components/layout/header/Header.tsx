2bf43eb1ef72cfdae6c9ec9039f5b68cfba3a149
Hubert <h.kliszcz@nebucode.com>
2023-07-21 14:36:23 +0200

    add input search and buttons my account and basket

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -2,7 +2,12 @@ import styles from './Header.module.scss';
 import Image from 'next/image';
 import nameLogo from '@/assets/image/logo-name.png';
 import dogLogo from '@/assets/image/logo-dog.png';
+import { useTranslation } from 'next-i18next';
+import { Button } from '@/components/base/button/Button';
+import { PROJECT_URL } from '@/const/project-url';
+
 export const Header = () => {
+  const { t } = useTranslation('header');
   return (
     <>
       <div className={styles.bar}></div>
@@ -12,10 +17,19 @@ export const Header = () => {
           <Image src={dogLogo} alt="Lakrama" width={200} height={100} />
         </div>
         <div>
-          <h1>szukajka</h1>
+          <input type="search" placeholder={t('search-placeholder')} />
         </div>
         <div>
-          <h1>logowanie</h1>
+          <div>
+            <Button
+              variant="link"
+              text={t('my-account')}
+              href={PROJECT_URL.myAccount}
+            />
+          </div>
+          <div>
+            <Button variant="link" text={t('basket')} href={'#'} />
+          </div>
         </div>
       </div>
       <div className={styles.menu}></div>
---
+++ src/const/project-url.ts
@@ -0,0 +1,3 @@
+export const PROJECT_URL = {
+  myAccount: '/moje-konto',
+};
