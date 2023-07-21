cc49c66cf98f95f0e85376e6cc7fee46b3c1db0e
Hubert <h.kliszcz@nebucode.com>
2023-07-21 16:03:24 +0200

    Add style to button component

--- src/components/templates/HomePage/HomePage.tsx
+++ src/components/templates/HomePage/HomePage.tsx
@@ -1,6 +1,5 @@
-import { Typography } from '@/components/base/typography/Typography';
 import { Button } from '@/components/base/button/Button';
 
 export const HomePage = () => {
-  return <Button text="Text" href="lala" />;
+  return <Button text="Text" variant="outlined" color="light-gray" />;
 };
