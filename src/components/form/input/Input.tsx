ff9c6b34aaabd10ea3e2f9cb1ffca27af4daa5ea
Hubert <h.kliszcz@nebucode.com>
2023-07-22 12:09:07 +0200

    add styles to input, option label to as in typography, input component with props and structure

--- src/components/form/input/Input.tsx
+++ src/components/form/input/Input.tsx
@@ -1,4 +1,41 @@
 import styles from './Input.module.scss';
-export const Input = () => {
-  return <></>;
+import { FC, InputHTMLAttributes } from 'react';
+import classNames from 'classnames';
+import { Typography } from '@/components/base/typography/Typography';
+
+export interface InputProps
+  extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
+  label?: string;
+  required?: boolean;
+  message?: string;
+  type?: 'text' | 'email' | 'password';
+}
+export const Input: FC<InputProps> = ({
+  label,
+  required,
+  message,
+  className,
+  type,
+  ...rest
+}) => {
+  return (
+    <div className={classNames(styles.inputWrapper, className)}>
+      {label && (
+        <div className={styles.labelWrapper}>
+          <Typography
+            as="label"
+            htmlFor={rest.name}
+            className={styles.label}
+            text={label}
+          />
+        </div>
+      )}
+      <input
+        type={type}
+        required={required}
+        className={styles.input}
+        {...rest}
+      />
+    </div>
+  );
 };
