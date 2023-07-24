359c756ca85a25d3eba832bba3f8453259d0d1ad
Hubert <h.kliszcz@nebucode.com>
2023-07-24 15:56:51 +0200

    add database users

---
+++ src/mocks/usersDatabase.ts
@@ -0,0 +1,17 @@
+export const usersDatabase = [
+  {
+    login: 'admin',
+    password: 'admin123',
+    email: 'admin@gmail.com',
+  },
+  {
+    login: 'tomek',
+    password: 'tomek123',
+    email: 'tomek@gmail.com',
+  },
+  {
+    login: 'magda',
+    password: 'magda123',
+    email: 'magda@gmail.com',
+  },
+];
