17d6071b092d7b3d92ed167358918e97752ca8bf
Hubert <h.kliszcz@nebucode.com>
2023-07-25 19:18:49 +0200

    Add Bar info to Header, and styles Typografy to isUppercase props

--- src/components/layout/header/bar/Bar.tsx
+++ src/components/layout/header/bar/Bar.tsx
@@ -3,9 +3,26 @@ import { Typography } from '@/components/base/typography/Typography';
 import styles from './Bar.module.scss';
 
 export const Bar = () => {
+  const information = [
+    'Darmowa dostawa od 99zł',
+    'Promocja! Na 3 sztukę 50% rabatu',
+    'Zapisz sie do newslettera i otrzymaj 10% rabatu',
+    'Najniższe ceny',
+  ];
+
+  const infoBar = new Array(20);
+  infoBar.fill(information);
+  infoBar.flat(2);
+
+  console.log(infoBar);
+
   return (
     <div className={styles.bar}>
-      <Typography as="span" text="Darmowa dostawa od 99zł" />
+      {infoBar.map((info) =>
+        info.map((i: string) => (
+          <Typography as="p" text={i} capitalization="2xs" isUppercase />
+        )),
+      )}
     </div>
   );
 };
