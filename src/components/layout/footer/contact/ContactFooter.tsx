275aaafc04407134beeda53797282f387d4a00d1
Hubert <h.kliszcz@nebucode.com>
2023-07-31 15:17:29 +0200

    add image and email contact to footer contact

--- src/components/layout/footer/contact/ContactFooter.tsx
+++ src/components/layout/footer/contact/ContactFooter.tsx
@@ -1,6 +1,9 @@
 import styles from './ContactFooter.module.scss';
 import { Typography } from '@/components/base/typography/Typography';
 import { useTranslation } from 'next-i18next';
+import Image from 'next/image';
+
+import vet from '@/assets/image/vet.jpg';
 export const ContactFooter = () => {
   const { t } = useTranslation('contact-footer');
   return (
@@ -10,22 +13,42 @@ export const ContactFooter = () => {
         text={t('contact')}
         font="roboto"
         weight="500"
-        capitalization="md"
+        capitalization="lg"
       />
-      <Typography
-        as="p"
-        text={t('service')}
-        font="ptSans"
-        capitalization="xs"
-      />
-      <Typography
-        as="p"
-        text={t('contact-phone')}
-        font="ptSans"
-        capitalization="xs"
-        icon="Phone"
-        className={styles.contact}
-      />
+      <div>
+        <Typography
+          as="p"
+          text={t('service')}
+          font="ptSans"
+          capitalization="sm"
+        />
+        <Typography
+          as="p"
+          text={t('contact-phone')}
+          font="ptSans"
+          capitalization="sm"
+          icon="Phone"
+          className={styles.contact}
+        />
+      </div>
+
+      <div>
+        <Typography
+          as="p"
+          text={t('write-us')}
+          font="ptSans"
+          capitalization="sm"
+        />
+        <Typography
+          as="p"
+          text={t('contact-email')}
+          font="ptSans"
+          capitalization="sm"
+          icon="Envelope"
+          className={styles.contact}
+        />
+        <Image src={vet} alt="VET" width={100} className={styles.vet} />
+      </div>
     </div>
   );
 };
