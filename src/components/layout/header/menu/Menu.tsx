d0935f0b9a186c0a555669df2aef0d05a005ec36
Hubert <h.kliszcz@nebucode.com>
2023-08-01 17:34:19 +0200

    add base menu

--- src/components/layout/header/menu/Menu.tsx
+++ src/components/layout/header/menu/Menu.tsx
@@ -1,6 +1,8 @@
 import styles from './Menu.module.scss';
 import { useTranslation } from 'next-i18next';
 import { PROJECT_URL } from '@/const/project-url';
+import Link from 'next/link';
+import { Button } from '@/components/base/button/Button';
 export const Menu = () => {
   const { t } = useTranslation('menu');
 
@@ -11,9 +13,26 @@ export const Menu = () => {
     { text: t('birds'), link: PROJECT_URL.menu.birds, items: [] },
     { text: t('top-brand'), link: PROJECT_URL.menu.topBrand, items: [] },
     { text: t('sale'), link: PROJECT_URL.menu.sale, items: [] },
-    { text: t('pharmacy'), link: PROJECT_URL.menu.pharmacy, items: [] },
-    { text: t('treat'), link: PROJECT_URL.menu.treat, items: [] },
+    { text: t('pharmacy'), link: PROJECT_URL.menu.pharmacy },
+    { text: t('blog'), link: PROJECT_URL.menu.blog },
+    { text: t('treat'), link: PROJECT_URL.menu.treat },
   ];
 
-  return <div className={styles.wrapper}></div>;
+  return (
+    <div className={styles.wrapper}>
+      <ul className={styles.menu}>
+        {menu.map((i) => (
+          <li key={i.text} className={styles['menu-item']}>
+            <Button
+              text={i.text}
+              href={i.link}
+              textSize="base"
+              textColor={i.text === t('treat') ? 'white' : 'gray'}
+              className={styles.link}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
